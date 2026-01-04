2f093b21928b96d3b3dd2651f21a1aca26f6c724
Vidovic Branislav <branislav.vidovic@ext.a1.at>
2026-01-04 19:20:20 +0100

    cv splitting with test

--- src/cv_splitter.py
+++ src/cv_splitter.py
@@ -14,30 +14,25 @@ class CvSplitter:
         root_node = CVNode()
         last_2nd_level_node = CVNode()
 
+        current_node = None
         for line in lines:
-            current_node = CVNode()
             if line.strip().startswith('###'):
-                current_node.title = line.strip('###').strip()
+                current_node = CVNode()
+                current_node.title = line.strip()
                 current_node.parent = last_2nd_level_node
                 last_2nd_level_node.children.append(current_node)
             elif line.strip().startswith('##'):
-                current_node.title = line.strip('##').strip()
+                current_node = CVNode()
+                current_node.title = line.strip()
                 current_node.parent = root_node
                 root_node.children.append(current_node)
                 last_2nd_level_node = current_node
             elif line.strip().startswith('#'):
-                current_node.title = line.strip('#').strip()
+                current_node = CVNode()
+                current_node.title = line.strip()
                 current_node.parent = None
                 root_node = current_node
-
-            current_node.text += line.strip() + ' '
+            else:
+                current_node.text += line.strip() + ' '
 
         return root_node
-
-
-with open("../cv/Extended_CV.md", "r", encoding="utf-8") as f:
-    content = f.read()
-
-splitter = CvSplitter(content)
-tree = splitter.build_doc_tree()
-print(tree)
---
+++ src/cv_splitter_test.py
@@ -0,0 +1,47 @@
+from src.cv_splitter import CvSplitter
+
+def test_simple_content():
+    # prepare
+    content = """
+    # My CV
+    
+    ## Education
+    
+    ### School
+    Went to school
+    
+    ### University
+    University in Zrenjanin
+    
+    ## Experience
+    Company 1 - good projects
+    Company 2 - bad projects
+    
+    ## Highlights
+    Communicates with Cats.
+    Talks to cows.
+    """
+
+    # tests
+    splitter = CvSplitter(content)
+    tree = splitter.build_doc_tree()
+
+    #verify
+    assert len(tree.children) == 3
+    assert tree.title == "# My CV"
+
+    assert tree.children[0].title == "## Education"
+
+    assert tree.children[0].children[0].title == "### School"
+    assert tree.children[0].children[0].text.strip() == "Went to school"
+
+    assert tree.children[0].children[1].title == "### University"
+    assert tree.children[0].children[1].text.strip() == "University in Zrenjanin"
+
+    assert tree.children[1].title == "## Experience"
+    assert tree.children[1].text.strip() == 'Company 1 - good projects Company 2 - bad projects'
+
+    assert tree.children[2].title == "## Highlights"
+    assert tree.children[2].text.strip() == 'Communicates with Cats. Talks to cows.'
+
+test_simple_content()
