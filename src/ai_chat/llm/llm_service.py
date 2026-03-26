c5f8fe41a658fde1b6490481dbad46f4d1f72150
Vidovic Branislav <branislav.vidovic@ext.a1.at>
2026-03-26 11:58:30 +0100

    skills service

--- src/ai_chat/llm/llm_service.py
+++ src/ai_chat/llm/llm_service.py
@@ -46,3 +46,49 @@ Question: {question}<|end|>
         )
 
         return final_response
+
+    def answer_skills(self, question: str, context: str) -> str:
+        final_prompt = f"""<|system|>
+You are a precise assistant.
+Rules:
+Answer ONLY using the provided context.
+Do NOT mention "context" or explain your reasoning.
+Be concise and natural.
+For yes/no questions, answer with "Yes" or "No" followed by a short sentence.
+For list questions, return only the list.
+If the question can be answered partially mention exactly what information you can answer and what information is not known.
+Examples:
+Question: Has he used Maven?
+Answer: Yes, he has worked with Maven.
+Question: Did he work with Python and Maven?
+Answer: Yes, he has experience with both Python and Maven.
+Question: List his skills
+Answer: Java, Spring Boot, Hibernate, Maven
+<|end|>
+<|user|>
+Context: {context}
+Question: {question}
+<|end|>
+<|assistant|>
+"""
+
+        with measure_time() as elapsed_time:
+            response = self.generator_pipeline(final_prompt)
+
+        if len(response) == 0:
+            return "no_answer"
+
+        generated_text = response[0]["generated_text"]
+        if generated_text.find("<|assistant|>") == -1:
+            return "no_answer"
+
+        final_response = generated_text.split("<|assistant|>")[1].strip()
+        self.log.info(
+            "llm.service.response",
+            in_tokens=len(self.generator_pipeline.tokenizer.encode(final_prompt)),
+            out_tokens=len(self.generator_pipeline.tokenizer.encode(final_response)),
+            model=self.generator_pipeline.model.config.name_or_path,
+            duration_ms=round(elapsed_time())
+        )
+
+        return final_response
--- src/ai_chat/service/skills_service.py
+++ src/ai_chat/service/skills_service.py
@@ -1,3 +1,26 @@
+import structlog
+
+from ai_chat.llm.llm_service import LLMService
+from ai_chat.vectordb.cv_repository import CvRepository
+
+log = structlog.get_logger()
+
+
 class SkillsService:
-    def handle(self):
-        pass
+    def __init__(self, repository: CvRepository, llm_service: LLMService):
+        self.repository = repository
+        self.llm_service = llm_service
+
+    def handle(self, question: str) -> str:
+        skills_metadata = {"entityType": "skills"}
+        result = self.repository.metadata_query(skills_metadata)
+        log.info(
+            "cv.retrieval.metadata.skills",
+            query=skills_metadata,
+            doc_ids=[r.id for r in result],
+            path=[r.metadata["path"] for r in result],
+            category=[r.metadata["category"] for r in result]
+        )
+
+        context = "\n".join([r.document for r in result])
+        return self.llm_service.answer_skills(question, context)
